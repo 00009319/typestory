843aa6102b698acc0ba6ad480eeb4997874a90d9
jakhongirs <j.sagdullaev31@gmail.com>
2021-04-07 00:24:24 +0500

    Completed articles and create pages

--- index.js
+++ index.js
@@ -10,9 +10,43 @@ app.set('view engine', 'ejs')
 //Middlewares
 app.use('/static', express.static('public'))
 
+//Home Page
 app.get('/', (req, res) => {
   res.render('index')
 });
+
+//Create Page
+app.get('/create', (req, res) => {
+  res.render('create')
+});
+
+//Get date function
+var dt = new Date();
+const currentDate = dt.getFullYear() + "/" + (dt.getMonth() + 1) + "/" + dt.getDate();
+
+const articles = [
+
+  {
+    "title": "asdbsa",
+    "desc": "sdasd",
+    "date": currentDate,
+    "author": "jakhongir"
+  },
+
+  {
+    "title": "Dasturchi bo'lmang",
+    "desc": "sdasd",
+    "date": currentDate,
+    "author": "Azimjon Po'latov"
+  }
+  
+]
+
+//Articles Page
+app.get('/articles', (req, res) => {
+  res.render('articles', {articles: articles})
+});
+
 
 
 
