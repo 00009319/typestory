41186ab002c845298451f2300b7783e8fdd699a8
jakhongirs <j.sagdullaev31@gmail.com>
2021-04-07 22:08:18 +0500

    Completed create, post, readme

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# typestory
-Web Technologies CW
+# Typestory
+
+### This web application allows users to create, edit, delete notes.
+
+To run this web application locally you must follow the steps indicated below:
+
+1. Clone this repository
+```bash
+git clone https://github.com/00009319/typestory.git
+```
+
+2. Install dependicies
+```bash
+cd [project folder]
+npm install
+```
+
+3. Run the application
+```bash
+npm run dev
+```
+
+### Web application dependicies
+- express.js
+- ejs
+- nodemon
+- uuid
+
+### Web application repository on GitHub
+[link to GitHub repo](https://github.com/00009319/typestory.git)
+
--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 const express = require('express')
 const ejs = require('ejs')
 const app = express()
+const fs = require('fs')
+const path = require("path")
+const { v4 } = require("uuid")
 
 //EJS
 app.engine('ejs', ejs.renderFile)
@@ -9,47 +12,115 @@ app.set('view engine', 'ejs')
 
 //Middlewares
 app.use('/static', express.static('public'))
-
-//Home Page
-app.get('/', (req, res) => {
-  res.render('index')
-});
-
-//Create Page
-app.get('/create', (req, res) => {
-  res.render('create')
-});
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
 //Get date function
 var dt = new Date();
 const currentDate = dt.getFullYear() + "/" + (dt.getMonth() + 1) + "/" + dt.getDate();
 
-const articles = [
+/* File System */
+const filePath = path.join('./data', "articles.json")
+const read = fs.readFile
+const write = fs.writeFile
 
-  {
-    "title": "asdbsa",
-    "desc": "sdasd",
-    "date": currentDate,
-    "author": "jakhongir"
-  },
+//Home Page
+app.get('/', (req, res) => {
+  read(filePath, (err, data) => {
+    if(err) throw err
 
-  {
-    "title": "Dasturchi bo'lmang",
-    "desc": "sdasd",
-    "date": currentDate,
-    "author": "Azimjon Po'latov"
+    const posts = JSON.parse(data)
+
+    res.render('index', {articles: posts})
+  })
+
+});
+
+//Create Page
+app.get('/create', (req, res) => {
+  res.render('create', { error: null })
+});
+
+app.post('/create', async(req, res) => {
+  const title = req.body.title
+  const desc = req.body.desc
+  const author = req.body.author
+
+  if(title.trim() === "" || desc.trim() === "" || author.trim() === ""){
+    res.render('create', { error: true })
+  } else{
+    read(filePath, (err, data) => {
+      if(err) throw err
+
+      const posts = JSON.parse(data)
+
+      posts.unshift(
+        {
+          id: v4(), 
+          title: title,
+          desc: desc,
+          author: author,
+          date: currentDate
+        }
+      )
+
+      write(filePath, JSON.stringify(posts, null, 4), err => {
+        if(err) throw err
+
+        res.render('index', {articles: posts})
+      })
+    })
   }
-  
-]
+});
 
 //Articles Page
 app.get('/articles', (req, res) => {
-  res.render('articles', {articles: articles})
+  read(filePath, (err, data) => {
+    if(err) throw err
+
+    const posts = JSON.parse(data)
+
+    res.render('articles', {articles: posts})
+  })
+
 });
 
+//Detail Page
+app.get('/:id', (req, res) => {
+  const id = req.params.id
 
+  read(filePath, (err, data) => {
+    if(err) throw err
 
+    const posts = JSON.parse(data)
 
+    const post = posts.filter(note => note.id === id)[0]
+
+    res.render('detail', { post: post })
+  })
+});
+
+//Delete
+/* app.delete("/delete/:id", async (req, res) => {
+  const content = await read(filePath, "utf8")
+  const articles = JSON.parse(content)
+
+  const articleId = articles.findIndex(article => article.id === req.params.id)
+  
+  
+  if(articleId > -1){
+    const article = articles[articleId]
+    articles.splice(articles, 1)
+
+    await write(filePath, JSON.stringify(articles))
+
+    res.send('articles')
+  }
+  else{
+    res.send("error")
+  }
+  
+}) */
 
 
 //Server
